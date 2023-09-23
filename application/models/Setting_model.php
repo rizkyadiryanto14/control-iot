16c0aee60f579b63d40896afe672c26d3646b725
rizkyadiryanto14 <adiryantorizky140820@gmail.com>
2023-09-23 11:11:47 +0800

    fix : fix bug add setting and update setting and database

---
+++ application/models/Setting_model.php
@@ -0,0 +1,20 @@
+<?php
+
+class Setting_model extends CI_Model
+{
+	public function getSetting()
+	{
+		return $this->db->get('setting')->result();
+	}
+
+	public function insertSetting($data)
+	{
+		return $this->db->insert('setting', $data);
+	}
+
+	public function updateSetting($id,$data)
+	{
+		$this->db->where('id', $id);
+		return $this->db->update('setting', $data);
+	}
+}
